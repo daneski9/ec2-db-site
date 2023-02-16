ab00ee5173498b16ea59d39b49ac212fd0144eee
daneski9 <91101595+daneski9@users.noreply.github.com>
2023-02-16 02:21:16 -0800

    Update dbinfo.php

--- includes/dbinfo.php
+++ includes/dbinfo.php
@@ -1,8 +1,3 @@
-<?php
-$db_host = "us-cdbr-east-06.cleardb.net";
-$db_user = "b800f6d94d478e";
-$db_pass = "40f775a2";
-$db_name = "heroku_e56a44749efb109";
 
-$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
+***
